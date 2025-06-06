76a8e23ac1eb7803fe2c93be74b51291de1d6c15
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-06-06 16:43:44 -0300

    feat: created app-invoices and generate the migrations

---
+++ app-invoices/src/http/server.ts
@@ -0,0 +1,25 @@
+import fastifyCors from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, { origin: '*' })
+
+app.get('/health', () => {
+    return 'OK'
+})
+
+app.listen({
+    host: '0.0.0.0',
+    port: 3333
+}).then(() => {
+    console.log('[Invoices] HTTP Server is running on http://localhost:3333')
+})
