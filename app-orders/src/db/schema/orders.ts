a0629e71c7428b67dc6a2d761c0a56318405f062
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-06-06 15:57:43 -0300

    feat: save orders in db

--- app-orders/src/db/schema/orders.ts
+++ app-orders/src/db/schema/orders.ts
@@ -3,6 +3,7 @@ import { timestamp } from 'drizzle-orm/pg-core'
 import { pgEnum } from 'drizzle-orm/pg-core'
 import { text } from 'drizzle-orm/pg-core'
 import { pgTable } from 'drizzle-orm/pg-core'
+import { customers } from './customers.ts'
 
 export const orderStatus = pgEnum('order_status', [
     'pending',
@@ -13,7 +14,7 @@ export const orderStatus = pgEnum('order_status', [
 
 export const orders = pgTable('orders', {
     id: text().primaryKey(),
-    customerId: text().notNull(),
+    customerId: text().notNull().references(() => customers.id),
     amount: integer().notNull(),
     status: orderStatus().notNull().default('pending'),
     createdAt: timestamp().defaultNow().notNull()
--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -6,11 +6,17 @@ import {
 } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { channels } from '../broker/channels/index.ts'
+import fastifyCors from '@fastify/cors'
+import { schema } from '../db/schema/index.ts'
+import { db } from '../db/client.ts'
+import { randomUUID } from 'node:crypto'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, { origin: '*' })
 
 app.get('/health', () => {
     return 'OK'
@@ -27,7 +33,17 @@ app.post('/orders', {
 
     console.log('New order received:', amount)
 
-    channels.orders.sendToQueue('orders', Buffer.from('Testing order'))
+    channels.orders.sendToQueue('orders', Buffer.from(JSON.stringify({ amount })))
+
+    try {
+        await db.insert(schema.orders).values({
+            id: randomUUID(),
+            customerId: '39c2dbbe-ea6b-414a-9b48-52060051e92c',
+            amount,
+        })
+    } catch (error) {
+        console.log(error)
+    }
 
     return reply.status(201).send()
 })
