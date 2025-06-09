6458461f99d33b82715f70dd4e067b1aa5c48462
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-06-09 15:28:27 -0300

    build: setup traces with grafana

--- infra/src/services/orders.ts
+++ infra/src/services/orders.ts
@@ -1,8 +1,8 @@
 import * as awsx from '@pulumi/awsx'
 import * as pulumi from '@pulumi/pulumi'
-import { ordersDockerImage } from '../images/orders'
 import { cluster } from '../cluster'
-import { rabbitMQAdminHttpListener } from './rabbitmq'
+import { ordersDockerImage } from '../images/orders'
+import { amqpListener } from './rabbitmq'
 
 export const ordersService = new awsx.classic.ecs.FargateService('fargate-orders', {
     cluster,
@@ -16,11 +16,35 @@ export const ordersService = new awsx.classic.ecs.FargateService('fargate-orders
             environment: [
                 {
                     name: 'BROKER_URL',
-                    value: pulumi.interpolate`amqp://admin:admin@${rabbitMQAdminHttpListener.endpoint.hostname}:${rabbitMQAdminHttpListener.endpoint.port}`
+                    value: pulumi.interpolate`amqp://admin:admin@${amqpListener.endpoint.hostname}:${amqpListener.endpoint.port}`
                 },
                 {
                     name: 'DATABASE_URL',
                     value: 'postgresql://orders_owner:npg_secretHere.us-east-1.aws.neon.tech/orders?sslmode=require'
+                },
+                {
+                    name: 'OTEL_TRACES_EXPORTER',
+                    value: 'otlp'
+                },
+                {
+                    name: 'OTEL_EXPORTER_OTLP_ENDPOINT',
+                    value: 'https://otlp-gateway-prod-sa-east-1.grafana.net/otlp'
+                },
+                {
+                    name: 'OTEL_EXPORTER_OTLP_HEADERS',
+                    value: 'Authorization=Basic secretHere'
+                },
+                {
+                    name: 'OTEL_SERVICE_NAME',
+                    value: 'orders'
+                },
+                {
+                    name: 'OTEL_RESOURCE_ATTRIBUTES',
+                    value: 'service.name=orders,service.namespace=microservices,deployment.environment=production'
+                },
+                {
+                    name: 'OTEL_NODE_RESOURCE_DETECTORS',
+                    value: 'env,host,os'
                 }
             ],
         },
