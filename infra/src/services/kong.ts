66346a6ff7667fe25ef831cf105a31f58952c9a8
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-06-09 16:46:23 -0300

    chore: created listener in kong service

--- infra/src/services/kong.ts
+++ infra/src/services/kong.ts
@@ -3,6 +3,52 @@ import * as pulumi from '@pulumi/pulumi'
 import { cluster } from '../cluster'
 import { kongDockerImage } from '../images/kong'
 import { ordersHttpListener } from './orders'
+import { appLoadBalancer } from '../load-balancer'
+
+const proxyTargetGroup = appLoadBalancer.createTargetGroup('proxy-target', {
+    port: 8000,
+    protocol: 'HTTP',
+    healthCheck: {
+        path: '/orders/health',
+        protocol: 'HTTP',
+    },
+})
+
+export const proxyHttpListener = appLoadBalancer.createListener('proxy-http', {
+    port: 80,
+    protocol: 'HTTP',
+    targetGroup: proxyTargetGroup,
+})
+
+const adminTargetGroup = appLoadBalancer.createTargetGroup('admin-target', {
+    port: 8002,
+    protocol: 'HTTP',
+    healthCheck: {
+        path: '/',
+        protocol: 'HTTP',
+    },
+})
+
+export const adminHttpListener = appLoadBalancer.createListener('admin-http', {
+    port: 8002,
+    protocol: 'HTTP',
+    targetGroup: adminTargetGroup,
+})
+
+const adminAPITargetGroup = appLoadBalancer.createTargetGroup('admin-api-target', {
+    port: 8001,
+    protocol: 'HTTP',
+    healthCheck: {
+        path: '/',
+        protocol: 'HTTP',
+    },
+})
+
+export const adminAPIHttpListener = appLoadBalancer.createListener('admin-api-http', {
+    port: 8001,
+    protocol: 'HTTP',
+    targetGroup: adminAPITargetGroup,
+})
 
 export const kongService = new awsx.classic.ecs.FargateService('fargate-kong', {
     cluster,
@@ -13,7 +59,11 @@ export const kongService = new awsx.classic.ecs.FargateService('fargate-kong', {
             image: kongDockerImage.ref,
             cpu: 256,
             memory: 512,
-            portMappings: [],
+            portMappings: [
+                proxyHttpListener,
+                adminHttpListener,
+                adminAPIHttpListener
+            ],
             environment: [
                 {
                     name: 'KONG_DATABASE',
