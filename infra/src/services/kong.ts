489a39b62e48c4c0180cebac85b9a78cced070d2
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-06-09 16:37:41 -0300

    chore: created kong service

---
+++ infra/src/services/kong.ts
@@ -0,0 +1,33 @@
+import * as awsx from '@pulumi/awsx'
+import * as pulumi from '@pulumi/pulumi'
+import { cluster } from '../cluster'
+import { kongDockerImage } from '../images/kong'
+import { ordersHttpListener } from './orders'
+
+export const kongService = new awsx.classic.ecs.FargateService('fargate-kong', {
+    cluster,
+    desiredCount: 1,
+    waitForSteadyState: false,
+    taskDefinitionArgs: {
+        container: {
+            image: kongDockerImage.ref,
+            cpu: 256,
+            memory: 512,
+            portMappings: [],
+            environment: [
+                {
+                    name: 'KONG_DATABASE',
+                    value: 'off'
+                },
+                {
+                    name: 'KONG_ADMIN_LISTEN',
+                    value: '0.0.0.0:8001'
+                },
+                {
+                    name: 'ORDERS_SERVICE_URL',
+                    value: pulumi.interpolate`http://${ordersHttpListener.endpoint.hostname}:${ordersHttpListener.endpoint.port}` //pulumi.secret('kong-password')
+                }
+            ],
+        },
+    },
+})
